86cac104170d8b7afd3680242b6a3d2950c60903
Liza <lnisse23@student.aau.dk>
2024-11-04 19:56:49 +0100

    Updated UI with Vaadin

---
+++ src/main/java/com/p3/solution/views/booking/BookingView.java
@@ -0,0 +1,127 @@
+package com.p3.solution.views.booking;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.Composite;
+
+import static org.apache.el.lang.ELArithmetic.add;
+
+@PageTitle("Booking")
+@Route("booking")
+public class BookingView extends Composite<VerticalLayout> {
+
+    public BookingView() {
+        VerticalLayout rootLayout = getContent(); // Main root layout, directly accessed via Composite
+
+        rootLayout.setWidthFull(); // Set the width of the root layout to 100%
+        rootLayout.setPadding(false); // Optional: Remove padding if you want it to look cleaner
+
+        // Top navigation bar with icon on the left and links + button on the right
+        HorizontalLayout topNavBar = new HorizontalLayout();
+        topNavBar.setWidthFull();
+        topNavBar.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        // Left section (icon)
+        HorizontalLayout leftIconLayout = new HorizontalLayout();
+        Image bikeIcon = new Image("icons/bike-icon.svg", "Bike Icon"); // Load custom bike icon
+        bikeIcon.setWidth("24px");
+        bikeIcon.setHeight("24px");
+        leftIconLayout.add(bikeIcon);
+
+        // Right section (links and button)
+        HorizontalLayout rightNavLayout = new HorizontalLayout();
+        Anchor link = new Anchor("#", "Forside");
+        Anchor link2 = new Anchor("#", "Kontakt");
+        link.getStyle().set("color", "green");
+        link2.getStyle().set("color", "green");
+        Button buttonPrimary = new Button("Book tid");
+        buttonPrimary.getStyle().set("background-color", "green").set("color", "white");
+        buttonPrimary.setWidth("min-content");
+
+        // Add links and button to the right layout
+        rightNavLayout.add(link, link2, buttonPrimary);
+        rightNavLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        // Combine left and right parts in the top navigation bar
+        topNavBar.add(leftIconLayout, rightNavLayout);
+        topNavBar.expand(leftIconLayout); // Push links + button to the far right
+
+        // Main content layout (Booking form and calendar)
+        HorizontalLayout contentLayout = new HorizontalLayout();
+        contentLayout.setWidthFull();
+        contentLayout.setAlignItems(FlexComponent.Alignment.START);
+
+        // Left section (Calendar and time slots)
+        VerticalLayout leftContentLayout = new VerticalLayout();
+        leftContentLayout.setWidth("50%");
+        leftContentLayout.setSpacing(true);
+
+        // Calendar
+        H2 calendarTitle = new H2("Vælg dato for reparation:");
+        DatePicker datePicker = new DatePicker();
+        datePicker.setWidthFull();
+        datePicker.setPlaceholder("Vælg dato");
+
+        // Time slots
+        Span timeSlotLabel = new Span("Du har valgt en dato, vælg en tid:");
+        Button timeSlot1 = new Button("08.00-10.00");
+        Button timeSlot2 = new Button("12.00-14.00");
+
+        // Style time slot buttons
+        timeSlot1.getStyle().set("background-color", "green").set("color", "white");
+        timeSlot2.getStyle().set("background-color", "green").set("color", "white");
+
+        // Add calendar and time slots to the left section
+        leftContentLayout.add(calendarTitle, datePicker, timeSlotLabel, timeSlot1, timeSlot2);
+
+        // Right section (Form for user details and bike info)
+        VerticalLayout rightContentLayout = new VerticalLayout();
+        rightContentLayout.setWidth("50%");
+        rightContentLayout.setSpacing(true);
+
+        // Form title
+        H2 formTitle = new H2("Dine oplysninger:");
+
+        // User Details Form
+        FormLayout formLayout = new FormLayout();
+        TextField firstName = new TextField("Fornavn*");
+        TextField lastName = new TextField("Efternavn*");
+        TextField phoneNumber = new TextField("Mobil nummer*");
+        EmailField email = new EmailField("Email*");
+
+        // Add form fields to form layout
+        formLayout.add(firstName, lastName, phoneNumber, email);
+
+        // Bike Information section
+        Span bikeInfoTitle = new Span("Cykel informationer:");
+        bikeInfoTitle.getElement().getStyle().set("font-weight", "bold");
+        TextArea commentField = new TextArea("Kommentarfelt*");
+        commentField.setPlaceholder("Skriv nærmere beskrivelse");
+
+        // Submit button
+        Button submitButton = new Button("Bestil");
+        submitButton.getStyle().set("background-color", "black").set("color", "white");
+
+        // Add form elements to the right section
+        rightContentLayout.add(formTitle, formLayout, bikeInfoTitle, commentField, submitButton);
+
+        // Combine left and right content sections in the main content layout
+        contentLayout.add(leftContentLayout, rightContentLayout);
+
+        // Add top navigation bar and main content layout to the root layout
+        rootLayout.add(topNavBar, contentLayout);
+    }
+}
---
+++ src/main/java/com/p3/solution/views/homepage/HomeView.java
@@ -0,0 +1,85 @@
+package com.p3.solution.views.homepage;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import com.vaadin.flow.component.html.Image;
+
+@PageTitle("Home")
+@Route("")
+@Menu(order = 0, icon = "line-awesome/svg/pencil-ruler-solid.svg")
+public class HomeView extends Composite<VerticalLayout> {
+
+    public HomeView() {
+        // Main layout that contains left and right sections
+        HorizontalLayout mainLayout = new HorizontalLayout();
+        mainLayout.setWidthFull();
+        mainLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        // Left section with icon
+        HorizontalLayout leftLayout = new HorizontalLayout();
+        Image bikeIcon = new Image("icons/bike-icon.svg", "Bike Icon"); // Load custom bike icon
+        bikeIcon.setWidth("24px");
+        bikeIcon.setHeight("24px");
+        leftLayout.add(bikeIcon); // Add the icon to the left layout
+
+        // Right section with links and button
+        HorizontalLayout rightLayout = new HorizontalLayout();
+        Anchor link = new Anchor("#", "Forside");
+        Anchor link2 = new Anchor("#", "Kontakt");
+        link.getStyle().set("color", "green");
+        link2.getStyle().set("color", "green");
+        Button buttonPrimary = new Button("Book tid");
+
+        // Style the button
+        buttonPrimary.getStyle().set("background-color", "green");
+        buttonPrimary.getStyle().set("color", "white");
+        buttonPrimary.setWidth("min-content");
+
+        // Add components to the right layout
+        rightLayout.add(link, link2, buttonPrimary);
+        rightLayout.setAlignItems(FlexComponent.Alignment.CENTER); // Center align links and button vertically
+
+        // Add both left and right layouts to the main layout
+        mainLayout.add(leftLayout, rightLayout);
+        mainLayout.expand(leftLayout); // Make left layout take up all available space on the left side
+
+        // Main content setup
+        H1 h1 = new H1("Dit professionelle cykelværksted");
+        h1.setWidth("max-content");
+
+        Paragraph textMedium = new Paragraph("Jeg er her for at hjælpe med alle typer cykler, så du hurtigt kan komme på vejen igen!");
+        textMedium.setWidth("100%");
+        textMedium.getStyle().set("font-size", "var(--lumo-font-size-m)");
+
+        // Secondary layout for additional buttons
+        HorizontalLayout layoutRow2 = new HorizontalLayout();
+        layoutRow2.setWidthFull();
+        layoutRow2.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        Button buttonPrimary2 = new Button("Book tid");
+        buttonPrimary2.getStyle().set("background-color", "green");
+        buttonPrimary2.getStyle().set("color", "white");
+        buttonPrimary2.setWidth("min-content");
+
+        Button buttonSecondary = new Button("Kontakt os");
+        buttonSecondary.getStyle().set("color", "green");
+        buttonSecondary.setWidth("min-content");
+
+        // Adding elements to the main layout
+        getContent().add(mainLayout, h1, textMedium, layoutRow2);
+        layoutRow2.add(buttonPrimary2, buttonSecondary);
+    }
+}
